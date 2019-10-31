5f6c2feb9f670bb2324a97b629b4b716017dc23a
Emilio Mariscal <emi420@gmail.com>
2019-10-31 12:13:49 -0300

    Deleted config

--- src/config/config.js
+++
@@ -1,3 +0,0 @@
-export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZW1pNDIwIiwiYSI6ImNqZW9leG5pZTAxYWwyeG83bHU0eHM0ZXcifQ.YWmk4Rp8FBGCLmpx_huJYw';
-export const MAPBOX_DEFAULT_STYLE = 'mapbox://styles/emi420/ck2divmvv1gip1cpjxumqy761';
-export const MAP_DEFAULT_CENTER = [-64.273833, -31.006031]
