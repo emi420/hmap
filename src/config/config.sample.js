9426e045bc6432bddbac114bc21bbf88c7f4205a
Emilio Mariscal <emi420@gmail.com>
2019-11-01 16:17:12 -0300

    FIRMS URLs

--- src/config/config.sample.js
+++ src/config/config.sample.js
@@ -1,3 +1,6 @@
 export const MAPBOX_ACCESS_TOKEN = '';
 export const MAPBOX_DEFAULT_STYLE = 'mapbox://styles/mapbox/satellite-streets-v9';
 export const MAP_DEFAULT_CENTER = [-64.273833, -31.006031]
+
+export const MODIS_24_URL = '/dev-data/MODIS_C6_South_America_24h.csv';
+export const VIIRS_24_URL = '/dev-data/VNP14IMGTDL_NRT_South_America_24h.csv';
