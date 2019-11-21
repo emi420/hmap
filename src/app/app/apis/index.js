a35f68f126d88ff54b87ba55ce05e877d79fa616
Emilio Mariscal <emi420@gmail.com>
2019-11-21 19:39:13 -0300

    - Remove unused comments, add some data utilities

--- src/app/app/apis/index.js
+++ src/app/app/apis/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-import { MODIS_24_URL, VIIRS_24_URL } from '../../../config/config';
+import { 
+    MODIS_24_URL, 
+    VIIRS_24_URL,
+} from '../../../config/config';
 
 const API = {
     getLatestModis24: function () {
@@ -12,43 +15,7 @@ const API = {
         return axios({
             url: VIIRS_24_URL
         })
-    }
+    },
 };
 
 export default API;
-
-/*
-
-// Buildings inside Colón, Córdoba
-
-[out:json];
-
-area[name="Córdoba"]->.b;
-rel(area.b)[name="Departamento Colón"];
-map_to_area -> .a;
-
-
-way["building"](area.a);
-out geom;
-
-relation["building"](area.a);out;
-way(r)[!"building:part"];
-out geom;
-*/
-
-// --------
-
-/*
-
-// Fire nodes
-
-[out:json];
-
-area[name="Córdoba"]->.b;
-
-node["emergency"~"^fire"](area.b);
-out;
-
-node[~"^fire_.*$"~"."](area.b);
-out;
-*/
---
+++ utils/data/download-firms.sh
@@ -0,0 +1,3 @@
+wget -O - https://firms.modaps.eosdis.nasa.gov/data/active_fire/c6/csv/MODIS_C6_South_America_24h.csv > ./MODIS_C6_South_America_24h.csv
+wget -O - https://firms.modaps.eosdis.nasa.gov/data/active_fire/viirs/csv/VNP14IMGTDL_NRT_South_America_24h.csv > ./VNP14IMGTDL_NRT_South_America_24h.csv
+
