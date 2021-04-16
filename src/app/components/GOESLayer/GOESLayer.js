d331b4ee84be9b312d3fd00cd8ef5e503666ccdc
Emillio Mariscal <emi420@gmail.com>
2021-04-16 13:23:39 -0300

    + GOES

---
+++ src/app/components/GOESLayer/GOESLayer.js
@@ -0,0 +1,13 @@
+
+const GOESLayer = (id, data) => {
+    return [{
+        id: id,
+        data: data,
+        circlePaint: {
+            "circle-radius": 4,
+            "circle-color": "yellow",
+        }
+    }];
+}
+
+export default GOESLayer;
