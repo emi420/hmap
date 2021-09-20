231e9071d9c8797abea974b35e17d963d28a879e
Pedro <pedro_defe@yahoo.com.ar>
2021-09-20 11:42:56 -0300

    FEATURE:  #5 MODELS: Create basic Layer model and its creation process
    - Modify precommit configuration for the check migrations hook.
    - - Create the layer model.
    Write a custom save method for the 'Layer' model
    - Add and register an admin class for the 'Layer' model.
    - update readme backend and front

--- hmap_backend/api/app/models.py
+++ hmap_backend/api/app/models.py
@@ -1,3 +1,32 @@
+import os
+
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
-# Create your models here.
+
+class Layer(models.Model):
+
+    name = models.TextField(null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    geojson_data = models.FileField(
+        upload_to="layers_geojson/", null=True, validators=[FileExtensionValidator(["geojson"])]
+    )
+    source_file = models.FileField(
+        upload_to="layer_source_files", validators=[FileExtensionValidator(["csv", "geojson", "kml"])]
+    )
+    style_file = models.FileField(null=True, blank=True)
+    is_public = models.BooleanField(default=False)
+    # created_by = models.ForeignKey(UserAccount, null=True, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
+
+    def save_geojson_data_from_source_file(self):
+        _, extension = os.path.splitext(self.source_file.name)
+        extension = extension.lower()
+        if extension == ".geojson":
+            self.geojson_data.save(self.source_file.name, self.source_file, save=False)
+
+    def save(self, *args, **kwargs):
+        self.save_geojson_data_from_source_file()
+        super().save(self, *args, **kwargs)
