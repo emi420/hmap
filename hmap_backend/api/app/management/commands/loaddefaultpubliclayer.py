c9c22b5b45467b57ca6e6ef466590d352f42a9f6
jmatias <juan.rizzi@solarsistemas.net>
2021-10-27 17:50:46 -0300

    - Create load default public layer. Source files in subdirectory. Delete all Layer, created in same command. - Change command load default public layer. Include style and all files for layers

--- hmap_backend/api/app/management/commands/loaddefaultpubliclayer.py
+++ hmap_backend/api/app/management/commands/loaddefaultpubliclayer.py
@@ -1,3 +1,4 @@
+import json
 import os
 import re
 
@@ -14,11 +15,12 @@ def delete_all():
     Layer.objects.filter(is_default_public_layer=True).delete()
 
 
-def create_layers(file):
+def create_layers(file, style_layer_file):
     filename_with_extension = os.path.basename(file.name)
     filename = filename_with_extension.split(".")[0]
     description = f"Public layer: {filename}"
-    layer = Layer(name=filename, description=description, is_public=True, is_default_public_layer=True)
+    style = json.load(style_layer_file)
+    layer = Layer(name=filename, description=description, is_public=True, is_default_public_layer=True, styles=style)
     layer.source_file.save(filename_with_extension, file)
 
 
@@ -28,6 +30,12 @@ def is_geojson(name_file):
     return bool(pattern.search(name_file))
 
 
+def parse_name_to_json(filename):
+    pattern = r"\."
+    name = re.split(pattern, filename)[0]
+    return f"{name}.json"
+
+
 class Command(BaseCommand):
     help = "Create layers publics from files geojson"
 
@@ -35,6 +43,9 @@ class Command(BaseCommand):
         files_gejson = [f_name for f_name in os.listdir(DEFAULT_PUBLIC_LAYER_DIR) if is_geojson(f_name)]
         delete_all()
         for filename in files_gejson:
-            with open(os.path.join(DEFAULT_PUBLIC_LAYER_DIR, filename)) as f:
+            name_json = parse_name_to_json(filename)
+            with open(os.path.join(DEFAULT_PUBLIC_LAYER_DIR, filename)) as f, open(
+                os.path.join(DEFAULT_PUBLIC_LAYER_DIR, name_json)
+            ) as style_layer_file:
                 file = File(f)
-                create_layers(file)
+                create_layers(file, style_layer_file),
