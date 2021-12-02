6f53de97588b4f6cd3ccae72b5255b10e3c22bac
Mariano Heredia <albolocura@gmail.com>
2021-12-02 19:03:03 -0300

    Updates

---
+++ precache-manifest.fa491d0a204d059f037f29fa1bf8a095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8fcf9e469d907655f4732229d77643",
+    "url": "/hmap/index.html"
+  },
+  {
+    "revision": "f3b86931e196cb47c657",
+    "url": "/hmap/static/css/2.d9a8a5a7.chunk.css"
+  },
+  {
+    "revision": "86c849dfa04e2087592c",
+    "url": "/hmap/static/css/main.932cefe1.chunk.css"
+  },
+  {
+    "revision": "f3b86931e196cb47c657",
+    "url": "/hmap/static/js/2.e61464b4.chunk.js"
+  },
+  {
+    "revision": "86c849dfa04e2087592c",
+    "url": "/hmap/static/js/main.6673fa40.chunk.js"
+  },
+  {
+    "revision": "01cbb41c152af5d792cb",
+    "url": "/hmap/static/js/runtime-main.85db6c28.js"
+  }
+]);
