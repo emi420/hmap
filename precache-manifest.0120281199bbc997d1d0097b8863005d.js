a813faa75013e6769e31170651f0a26e32ff655f
Emillio Mariscal <emi420@gmail.com>
2022-01-05 17:34:26 -0300

    Updates

---
+++ precache-manifest.0120281199bbc997d1d0097b8863005d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d84d38c744d0fdcf1b4e424b2279a6c",
+    "url": "/hmap/index.html"
+  },
+  {
+    "revision": "437fc578f60beaeb08b6",
+    "url": "/hmap/static/css/2.d9a8a5a7.chunk.css"
+  },
+  {
+    "revision": "17069b56e8dfe36e70b2",
+    "url": "/hmap/static/css/main.98518976.chunk.css"
+  },
+  {
+    "revision": "437fc578f60beaeb08b6",
+    "url": "/hmap/static/js/2.d94a6952.chunk.js"
+  },
+  {
+    "revision": "17069b56e8dfe36e70b2",
+    "url": "/hmap/static/js/main.7810be98.chunk.js"
+  },
+  {
+    "revision": "01cbb41c152af5d792cb",
+    "url": "/hmap/static/js/runtime-main.85db6c28.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12ec5bd6ccabac2821fefd1a00a6652a.js"
+  "/hmap/precache-manifest.0120281199bbc997d1d0097b8863005d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hmap/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
